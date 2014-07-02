9b46c1c9426ab6d19f2a3da7f826f14285ab2073
joeytall <joeytall.liu@gmail.com>
2014-07-02 13:21:21 -0700

    Another Big Change
    Change-Id: I87c04a041fa1f32aca1b8e30b1cf9ee57fcb80cb

--- Replace.py
+++ Replace.py
@@ -1,43 +1,61 @@
-import os, shutil, errno, datetime
+import os, shutil, errno, datetime, random
 
-def Main(path, count=0, problemfiles = []):
-  mkdir_p(path + "/backup")
+path, folderName, replaceString = "", "", ""
+
+def Main(pathFromUser, replaceStringFromUser):
+  global path, replaceString
+  path = pathFromUser
+  replaceString = replaceStringFromUser
+  count = 0
+  problemfiles = []
+  mkdir()
+
   for file in os.listdir(path):
     if file.endswith(".aspx.cs"):
-      shutil.copyfile(path + "/" + file, path + "/backup" + file)
-      if deleteSelected(file) == False:
+      shutil.copyfile(path + "/" + file, folderName + "/backup/" + file)
+      if replace(file, replaceString) == False:
         problemfiles.append(file)
       count+=1
     #if count == 1: break
+  print (folderName)
   print (str(count) + " files scanned!")
   print (str(len(problemfiles)) + " problem files found.")
   if len(problemfiles) != 0:
     print ("Problem Files: " + ', '.join(problemfiles))
   return
 
-def printSelected(file, match = False):
+def check():
+  file = random.choice(os.listdir(folderName + "/modified"))
   print (file)
-  with open("Codes/"+file) as f:
+  with open(folderName + "/modified/" + file) as f:
     for line in f:
       if 'Page_Init' in line:
-        match = True
-        next(f)
-        line = next(f)
-        while 'if' not in line:
-          line = next(f)
-        while '}' not in line:
+        while replaceString not in line:
           print(line)
           line = next(f)
-        print(line)
+        for n in range(3):
+          print(line)
+          line = next(f)
         return
-  if match == False:
-    return False
 
-def deleteSelected(file, match = False):
+def applyChanges():
+  for file in os.listdir(folderName + "/modified"):
+    shutil.copyfile(folderName + "/modified/" + file, path + "/" + file)
+  print ("Changes have been successfully Applied!")
+
+def undo():
+  for file in os.listdir(folderName + "/backup"):
+    shutil.copyfile(folderName + "/backup/" + file, path + "/" + file)
+  print ("Files have been restored!")
+
+def replace(file, replaceString):
+  match = False
   print (file)
-  w = open("new/" + file, "w")
-  with open("Codes/"+file) as f:
+  w = open(folderName + "/modified/" + file, "w")
+  with open(folderName + "/backup/"+file) as f:
     for line in f:
+      if 'framepanelcontrol' in line:
+        return False
       if 'Page_Init' in line:
         w.write(line)
         match = True
@@ -53,21 +71,22 @@ def deleteSelected(file, match = False):
           line = next(f)
         print(line)
         if '}' in line:
-          line = line.replace('}',"UserRights.CheckAccess('');")
+          line = line.replace('}', replaceString)
       w.write(line)
   if match == False:
     w.close()
     return False
 
-def mkdir_p():
+def mkdir():
     now = datetime.datetime.now()
-    folderName = now.strftime("%Y_%m_%d-%H%M")
+    global folderName
+    folderName = "result/" + now.strftime("%Y_%m_%d-%H%M")
+    print (folderName)
     try:
         os.makedirs(folderName)
-        os.makedirs(folderName + "/new")
+        os.makedirs(folderName + "/modified")
         os.makedirs(folderName + "/backup")
     except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
+        if exc.errno == errno.EEXIST and os.path.isdir(folderName):
+          pass
         else: raise
-        return folderName
--- test.py
+++
@@ -1,2 +0,0 @@
-def printmyself():
-    print ("I am included!")
