e3b59b487c3d6b797b6b906a6bf7af43681a429e
joeytall <joeytall.liu@gmail.com>
2014-08-03 14:23:27 -0700

    Organize Files
    Change-Id: Ic8433ecdbf42f2c2305c0c187c16d63eb21710d2

--- split.py
+++
@@ -1,9 +0,0 @@
-import os, datetime
-
-path = "my/little/pony.asdf"
-(filepath, filename) = os.path.split(path)
-
-print (filepath)
-print (filename)
-millis = datetime.datetime.now().microsecond
-print millis
